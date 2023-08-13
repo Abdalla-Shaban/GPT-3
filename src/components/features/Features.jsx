586a4c9c5cc68445918c1e39d49bf7b1d12f2851
abdalla <abdallashaban584@gmail.com>
2023-08-13 23:22:00 +0300

    implement featuers

---
+++ src/components/features/Features.jsx
@@ -0,0 +1,65 @@
+import "./features.css";
+function Features() {
+  return (
+    <div className="gpt3__features section__padding section__margin">
+      <span className="mask"></span>
+      <div className="gpt3__features-intro">
+        <h1 className="gradient__text">
+          The Future is Now and <br /> You Just Need To Realize <br /> It. Step
+          into Future Today <br /> & Make it Happen.
+        </h1>
+        <p>Request Early Access to Get Started</p>
+      </div>
+      <div className="gpt3__features-container">
+        <div>
+          <h3>
+            <span></span>Improving end <br /> distrusts instantly
+          </h3>
+          <div>
+            <p>
+              From they fine john he give of rich he. They age and draw mrs
+              like. Improving end distrusts may instantly was household
+              applauded.
+            </p>
+          </div>
+        </div>
+        <div>
+          <h3>
+            <span></span>Become the <br /> tended active
+          </h3>
+          <div>
+            <p>
+              Considered sympathize ten uncommonly occasional assistance
+              sufficient not. Letter of on become he tended active enable to.
+            </p>
+          </div>
+        </div>
+        <div>
+          <h3>
+            <span></span>Message or am <br /> nothing
+          </h3>
+          <div>
+            <p>
+              Led ask possible mistress relation elegance eat likewise debating.
+              By message or am nothing amongst chiefly address.
+            </p>
+          </div>
+        </div>
+        <div>
+          <h3>
+            <span></span>Really boy law <br /> county
+          </h3>
+          <div>
+            <p>
+              Really boy law county she unable her sister. Feet you off its like
+              like six. Among sex are leave law built now. In built table in an
+              rapid blush.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Features;
