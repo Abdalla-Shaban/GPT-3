f35a2169cc65bbd51d719db45cd647205c2e1f05
abdalla <abdallashaban584@gmail.com>
2023-08-13 23:22:30 +0300

    implement cta

---
+++ src/components/cta/Cta.jsx
@@ -0,0 +1,16 @@
+import "./cta.css";
+function Cta() {
+  return (
+    <div className="gpt3__cta section__margin">
+      <div className="gpt3__cta-text">
+        <p>Request Early Access to Get Started</p>
+        <h3>Register today & start exploring the endless possiblities.</h3>
+      </div>
+      <div className="gpt3__cta-start">
+        <button type="button">Get Started</button>
+      </div>
+    </div>
+  );
+}
+
+export default Cta;
