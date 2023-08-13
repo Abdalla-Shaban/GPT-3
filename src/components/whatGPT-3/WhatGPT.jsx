c59804f5c894c51c2c91a489d40359db2ce4a035
abdalla <abdallashaban584@gmail.com>
2023-08-13 23:21:39 +0300

    implement what is GPT-3 section

---
+++ src/components/whatGPT-3/WhatGPT.jsx
@@ -0,0 +1,63 @@
+import "./whatgpt.css";
+function WhatGPT() {
+  return (
+    <div className="gpt3__whatgpt section__padding section__margin">
+      <span className="mask-top"></span>
+      <span className="mask-bottom"></span>
+      <div className="gpt3__whatgpt-content">
+        <div className="gpt-3__whatgpt-content-intro">
+          <h1>
+            <span></span>What is GPT-3
+          </h1>
+          <p>
+            We so opinion friends me message as delight. Whole front do of plate
+            heard oh ought. His defective nor convinced residence own.
+            Connection has put impossible own apartments boisterous. At jointure
+            ladyship an insisted so humanity he. Friendly bachelor entrance to
+            on by.
+          </p>
+        </div>
+      </div>
+      <div className="gpt3__whatgpt-possibilities">
+        <div className="gpt3__whatgpt-possibilities-text">
+          <h1 className="gradient__text">
+            The possibilities are beyond
+            <br /> your imagination
+          </h1>
+          <p>Explore The Library</p>
+        </div>
+        <div className="gpt3__whatgpt-possibilities-container">
+          <div>
+            <h1>
+              <span></span>Chatbots
+            </h1>
+            <p>
+              We so opinion friends me message as delight. Whole front do of
+              plate heard oh ought.
+            </p>
+          </div>
+          <div>
+            <h1>
+              <span></span>Knowledgebase
+            </h1>
+            <p>
+              At jointure ladyship an insisted so humanity he. Friendly bachelor
+              entrance to on by. As put impossible own apartments b
+            </p>
+          </div>
+          <div>
+            <h1>
+              <span></span>Education
+            </h1>
+            <p>
+              At jointure ladyship an insisted so humanity he. Friendly bachelor
+              entrance to on by. As put impossible own apartments b
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default WhatGPT;
